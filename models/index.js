80af57fb6d89b442f399de69ba84b15271e61201
burtk33 <burtk33@gmail.com>
2021-03-23 20:41:39 -0400

    setting up relations for tables

--- models/index.js
+++ models/index.js
@@ -3,6 +3,42 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+Product.belongsTo(Category);
+
+Category.hasMany(Product);
+
+Product.belongsToMany(Tag,{
+    through:{
+      model: ProductTag,
+      unique: false
+    },
+    as: 'tags'
+});
+
+Tag.belongsToMany(Product,{
+  through:{
+    model: ProductTag,
+    unique: false
+  },
+  as: 'products'
+});
+
+Tag.hasMany(Product,{
+  through:{
+    model: ProductTag,
+    unique: false
+  },
+  as: 'products'
+});
+
+Product.hasMany(Tag,{
+  through:{
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tags'
+});
+
 
 
 module.exports = { Product, Category, ProductTag, Tag };
